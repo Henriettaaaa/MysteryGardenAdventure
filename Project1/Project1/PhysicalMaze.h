3e20b99fbd7f58adb01f187b4affd56fa745b48c
Henriettaaaa <1822466854@qq.com>
2025-04-01 19:30:28 +0800

    finish physical maze

---
+++ Project1/Project1/PhysicalMaze.h
@@ -0,0 +1,34 @@
+//迷宫的底层物理生成
+#ifndef PHYSICALMAZE_H
+#define PHYSICALMAZE_H
+
+#include <vector>
+#include <algorithm>
+#include <random>
+
+class PhysicalMaze{  //类定义的时候没有(),函数定义才需要()
+private:
+    const int size;  //通过带参构造进行初始化
+    std::vector<int> way;  //?用于存储迷宫路径
+    std::vector<int> parent;  //用于存储每个格子的根节点,自己就是根节点的，parent值=-1
+    std::vector<int> weight;  //用于存储每个格子的权重(用于带权重的合并)
+    std::vector<std::pair<int,int>> walls;  //用于存储所有墙
+
+public:
+    static int inputSize();  // 静态成员函数用于获取尺寸输入
+    explicit PhysicalMaze(int s);  //带参构造函数，并防止隐式转换
+    int getSize() const { return size; }   //获取迷宫尺寸
+
+    std::vector<std::pair<int,int>> generateWalls(int size);  // 生成墙
+    void generateMaze(int s);  // 生成迷宫
+    //void printMaze() const;  // 打印迷宫
+    //void solveMaze();  // 解决迷宫
+    //void printSolution() const;  // 打印解决方案
+
+    int find(int x);  // 查找根节点
+    void unionSet(int x, int y, int i);  // 合并
+
+    std::vector<int> findPath(int size);  // 得到迷宫走通的路径
+};
+
+#endif
